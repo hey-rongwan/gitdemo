b879b38ad1cfa6627f2943dd5000f0e645da78df
hey_rongwan <2695081432@.qq.com>
2020-03-27 12:58:25 +0800

    update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -9,3 +9,8 @@ for i in range(len(a)):   #遍历a列表中元素，
 print(len(b)-1)      #用len函数求列表b的长度，由于从0算起，减1得a中用到的字母种类数
 c=a.split()         #用split函数将a中的元素分块并组成列表c
 d=[]              #创建空列表d
+for j in range(len(c)):                      #遍历c中元素
+if c[j] not in ['AND','THE','FOR','OF']:    #将c中非“AND”,“THE”，“FOR”,“OF”的词块的首个字母添加进d中
+        d.append(c[j][0])
+for k in range(len(d)):                     #遍历并返回d中每一个元素
+    print(d[k],end='')
