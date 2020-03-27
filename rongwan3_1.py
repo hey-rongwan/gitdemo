59a7611ce2ab2d5d51ae74c4300f968a43b80982
hey_rongwan <2695081432@.qq.com>
2020-03-27 12:05:03 +0800

    add three files.py

--- rongwan3_1.py
+++ rongwan3_1.py
@@ -1,3 +1,13 @@
 #加密问题
 #创建者：荣婉
 #日期：2018.4.11
+a=list(input().split())        #输入原字符并返回为列表s
+b=list(input().split())        #输入原字符对应的密文并返回为列表b
+c={}                     #创建空白列表c
+for i in range(len(a)):        #用len函数计算并返回列表a的长度，遍历a中字符，
+    c[a[i]]=b[i]            #将a中i位置字符与b中i位置字符对应起来并储存在c   
+d=input()                 #输入要加密的字符并返回为列表d
+f=''                      #创建空字符串f
+for i in range(len(d)):        #遍历d中字符并将d中i位置字符赋值为a中相同字符所
+    f+='{}'.format(c[d[i]])   #相同字符所对应的b中字符，添加进f中
+print(f)                   #输出f，即得加密后字符串
--- rongwan3_3.py
+++ rongwan3_3.py
@@ -1,3 +1,16 @@
 #解决新北京地铁问题
 #创建：荣婉
 #日期：2018.4.11
+n1,n2,n3=map(int,input().split())    #用split函数单行输入三个分离整数
+a1=0                          #用a1、a2、a3分别表示第1、2、3天的乘车总价
+a2=0
+a3=0
+for i in range(1,n1+1):        #输入n1行数据，每行输入第一天单次乘车价格
+    i1=int(input())
+    a1=a1+i1               #将每一次的乘车价格加到a1，得到第一天的乘车总价
+for i in range(1,n2+1):
+    j1=int(input())
+    a2=a2+j1
+for i in range(1,n3+1):
+    k1=int(input())
+    a3=a3+k1
